83d7066a2a8e9495b7d1821bd14398712a2e1b1e
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-07-11 15:22:35 +0530

    Create newtesting.py

---
+++ newtesting.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+def read_file(filepath):
+    with open(filepath, 'r') as f:
+        content = f.read()
+    return content.splitlines
+
+def count_words(lines):
+    word_count = 0
+    for line in lines:
+        words = line.strip().split(" ")
+        word_count += len(words)
+    return word_count
+
+def main():
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print("File not found", filename)
+        exit
+    data = read_file(filename)
+    count = count_words(data)
+    print("Total words in file is: " + count)
+
+main()
