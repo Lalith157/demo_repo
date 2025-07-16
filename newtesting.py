db65b8046d3ef88d9ac52ebd3f5fe94a3fecd643
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-07-16 14:59:31 +0530

    Update newtesting.py

--- newtesting.py
+++ newtesting.py
@@ -1,11 +1,11 @@
 import sqlite3
 def get_user_data(username):
-    conn = sqlite3.connect("users.db")
+    conn = sqlite3.connect("users.db");
     cursor = conn.cursor()
     query = "SELECT * FROM users WHERE username = '" + username + "';"
     cursor.execute(query)
     result = cursor.fetchall()
-    conn..close()
+    conn.close()
     return result
     
-print(get_user_data(input("Enter username: ")))
+print(get_user_data(input("Enter username: ")));
