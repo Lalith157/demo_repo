ff35897f73c4d02d9d488176772fc8d1cdbea63e
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-07-21 16:37:17 +0530

    Update newtesting.py

--- newtesting.py
+++ newtesting.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/run', methods=['GET'])
-def run_command():='fg
+def run_command():='f
     cmd = request.args.get('cmd')  # ❌ No input validations
     os.system(cmd)  # ❌ Command Injection vulnerability
     return f"Executed: {cmd}"
