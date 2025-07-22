de4809af9949bc86c5d1ac76daf2015167f5e307
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-07-22 17:58:39 +0530

    Create c_testing.c

---
+++ c_testing.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char buf[8];                             // 🔸 Buffer too small
+    gets(buf);                               // 🔸 Vulnerable to buffer overflow
+    printf(buf);                             // 🔸 Format string vulnerability
+    char *ptr = malloc(10);
+    // missing free(ptr); 🔸 Memory leak
+    return 0;
+}
