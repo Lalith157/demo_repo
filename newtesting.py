b48e7a0b03603cf7f1126cd4afb44eb97cf3e00a
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-08-08 16:49:23 +0530

    Update newtesting.py

--- newtesting.py
+++ newtesting.py
@@ -1,4 +1,8 @@
-def connect_to_service():
-    username = "admin"
-    password = "supersecret"  # 🚨 Vulnerable: hardcoded passwordfgh
-    print(f"Connecting as {username}...")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/xss')
+def xss_vuln():
+    name = request.args.get("name")
+    return f"<html><body>Hello {name}</body></html>"
