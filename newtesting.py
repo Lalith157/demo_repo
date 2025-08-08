bc6677e09ed1a98a8776c875f1ecdf4e90ed31cc
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-08-08 16:38:55 +0530

    Update newtesting.py

--- newtesting.py
+++ newtesting.py
@@ -1,4 +1,4 @@
 def connect_to_service():
     username = "admin"
-    password = "supersecret"  # 🚨 Vulnerable: hardcoded password
+    password = "supersecret"  # 🚨 Vulnerable: hardcoded passwordfgh
     print(f"Connecting as {username}...")
