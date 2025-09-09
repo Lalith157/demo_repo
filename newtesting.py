b202b2f28e46bde143177f781b563b0f6baaf971
Lalith157 <112712199+Lalith157@users.noreply.github.com>
2025-09-09 13:32:37 +0530

    Update newtesting.py

--- newtesting.py
+++ newtesting.py
@@ -1,8 +1,1 @@
-username = "admin"
-password = "12345"
-
-def login(user, pwd):
-    if user == username and pwd == password:
-        print("Login successful!")
-    else:
-        print("Login failed!")
+/
